eae00e4d280190e9c1550bc50b4d5889d643dd34
Deokhun KIM <thekoon0456@gmail.com>
2024-02-09 01:40:49 +0900

    [Feat] DateFormatterManager 추가

---
+++ SeSAC_HolidaysAssignment/SeSAC_HolidaysAssignment/Manager/DateFormatterManager.swift
@@ -0,0 +1,45 @@
+//
+//  DateFormatterManager.swift
+//  SeSAC_HolidaysAssignment
+//
+//  Created by Deokhun KIM on 2/9/24.
+//
+
+import Foundation
+
+final class DateFormatterManager {
+    
+    enum DateStyle: String {
+        case threeHourCell = "yyyy-MM-dd HH:mm:ss"
+        
+        case hour = "HH시"
+    }
+    
+    static let shared = DateFormatterManager()
+    
+    private init() { }
+    
+    let formatter = DateFormatter()
+    private var krLocale = Locale(identifier: "ko_kr")
+    
+    // MARK: - 최종적인 format결과
+    
+    func formattedDate(input: String, inputFormat: DateStyle = .threeHourCell, outputFormat: DateStyle = .hour) -> String {
+        let date = stringToDate(input, format: inputFormat)
+        return dateToString(date, format: outputFormat)
+    }
+    
+    func dateToString(_ date: Date?, format: DateStyle) -> String {
+        formatter.locale = krLocale
+        formatter.dateFormat = format.rawValue
+        let result = formatter.string(from: date ?? Date())
+        return result
+    }
+    
+    private func stringToDate(_ stringDate: String, format: DateStyle) -> Date? {
+        formatter.locale = krLocale
+        formatter.dateFormat = format.rawValue
+        let result = formatter.date(from: stringDate)
+        return result
+    }
+}
